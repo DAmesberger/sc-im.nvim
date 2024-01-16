3eb8b7e54e39c30a722f6e256cd6f9badbc2d5c9
Daniel Amesberger <daniel.amesberger@amescon.com>
2024-01-16 15:23:53 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,112 @@
+
+# sc-im.vim - Neovim Plugin for sc-im Integration
+
+`sc-im.vim` is a Neovim plugin designed to seamlessly integrate `sc-im`, a terminal spreadsheet program to edit Markdown tables. It allows users to edit and insert `sc-im` tables directly within Neovim, enhancing the editing experience for Markdown files or other documents where `sc-im` tables are used.
+
+## Features
+
+- Open existing `sc-im` tables in a split window.
+- Insert new `sc-im` tables at the current cursor position.
+- Configurable to include `.sc` file links and customizable link text.
+
+## Installation
+
+You can install `sc-im.vim` using various plugin managers for Neovim. Below are examples for some common plugin managers:
+
+### Using [vim-plug](https://github.com/junegunn/vim-plug)
+
+Add the following line to your `init.vim`:
+
+```vim
+Plug 'DAmesberger/sc-im.vim'
+```
+
+Then run `:PlugInstall` in Neovim.
+
+### Using [packer.nvim](https://github.com/wbthomason/packer.nvim)
+
+Add the following Lua code to your Neovim configuration:
+
+```lua
+use 'DAmesberger/sc-im.vim'
+```
+
+### Using [dein.vim](https://github.com/Shougo/dein.vim)
+
+Add the following line to your `init.vim`:
+
+```vim
+call dein#add('DAmesberger/sc-im.vim')
+```
+
+### Using [Pathogen](https://github.com/tpope/vim-pathogen)
+
+Clone the repository into your `~/.config/nvim/bundle` directory:
+
+```sh
+cd ~/.config/nvim/bundle
+git clone https://github.com/DAmesberger/sc-im.vim.git
+```
+
+## Configuration
+
+`sc-im.vim` can be configured in your `init.vim` or Lua configuration file. Here is an example:
+
+```vim
+lua << EOF
+require'sc-im.vim'.setup({
+    include_sc_file = true,     -- Whether to include .sc file links
+    link_text = "Open .sc file" -- Custom text for .sc file links
+})
+EOF
+```
+
+## Usage
+
+### Opening an Existing `sc-im` Table
+
+Navigate to a line in a markdown file with an `sc-im` table and use the configured key mapping or command:
+
+```vim
+nnoremap <leader>sc :lua require'sc-im.vim'.OpenInScim()<CR>
+```
+
+### Inserting a New `sc-im` Table
+
+Place the cursor where you want a new table and use:
+
+```vim
+:lua require'sc-im.vim'.InsertNewScimTable({})
+```
+
+Or set a key mapping for convenience:
+
+```vim
+
+```vim
+nnoremap <leader>nc :lua require'sc-im.vim'.InsertNewScimTable({})<CR>
+```
+
+### Overriding Configuration for a Single Use
+
+To override the global configuration for a single use, pass a configuration table to `OpenInScim`:
+
+```vim
+:lua require'sc-im.vim'.OpenInScim({include_sc_file = false, link_text = "View .sc file"})
+```
+
+## Examples
+
+### Opening an `sc-im` Table
+
+1. In a markdown file, move the cursor over a line containing an `sc-im` table.
+2. Press the key mapping (e.g., `<leader>sc`) to open the table in `sc-im`.
+
+### Inserting a New Table
+
+1. Place the cursor at the desired location for a new table.
+2. Press the key mapping for inserting a new table (e.g., `<leader>nc`).
+
+## Contribution
+
+Contributions to `sc-im.vim` are welcome. Open an issue or pull request on the repository for any bugs, suggestions, or improvements.
---
+++ lua/sc-im-vim.lua
@@ -0,0 +1,171 @@
+local scim = {}
+
+-- Configuration options
+local config = {
+    include_sc_file = false, -- Do not include .sc file by default
+    link_text = "sc file"
+}
+
+local sc_file_link_pattern = "%[(.+)%]%((.+)%)"
+
+
+local function generate_uuid()
+    math.randomseed(os.time())
+    local random = math.random
+    local template = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'
+    return string.gsub(template, '[xy]', function(c)
+        local v = (c == 'x') and random(0, 0xf) or random(8, 0xb)
+        return string.format('%x', v)
+    end)
+end
+
+-- Internal function to read data back from sc-im
+local function read_from_scim(table_top_line, table_bottom_line, md_file, sc_file, effective_config)
+    -- Read the updated content from the markdown file
+    local md_content = vim.fn.readfile(md_file)
+
+    -- Determine the range of lines to replace, excluding the old .sc file link
+    local end_line = table_bottom_line
+    local next_line = vim.api.nvim_buf_get_lines(0, end_line, end_line + 1, false)[1] or ""
+    if next_line:match(sc_file_link_pattern) then
+        -- If the next line is an .sc file link, exclude it from the replacement range
+        end_line = end_line - 1
+    end
+
+    -- Replace the old table content in the buffer, excluding the .sc file link
+    vim.api.nvim_buf_set_lines(0, table_top_line - 1, end_line + 1, false, md_content)
+
+    -- If .sc file should be included, handle the .sc file link
+    if effective_config.include_sc_file then
+        local sc_link_line = table_top_line - 1 + #md_content
+        local sc_link = "[" .. effective_config.link_text .. "](" .. sc_file .. ")"
+        -- Replace or add the .sc file link
+        vim.api.nvim_buf_set_lines(0, sc_link_line, sc_link_line + 1, false, { sc_link })
+    end
+end
+
+-- Internal function to open the current table in sc-im
+local function open_in_scim(effective_config)
+    local cursor_line = vim.api.nvim_win_get_cursor(0)[1]
+    local line_content = vim.api.nvim_buf_get_lines(0, cursor_line - 1, cursor_line, false)[1]
+
+    -- Initialize table_top_line as nil
+    local table_top_line = nil
+
+    -- Find the top line of the table
+    while cursor_line > 0 and string.match(line_content, "|.*|$") do
+        cursor_line = cursor_line - 1
+        table_top_line = cursor_line + 1
+        if cursor_line > 0 then
+            line_content = vim.api.nvim_buf_get_lines(0, cursor_line - 1, cursor_line, false)[1]
+        end
+    end
+
+    -- If no table is found, do not proceed
+    if not table_top_line then
+        print("No table found under the cursor, creating new one.")
+        table_top_line = cursor_line
+    end
+
+    local file_lines = {}
+    cursor_line = table_top_line
+
+    -- Collect table lines
+    while true do
+        local lines = vim.api.nvim_buf_get_lines(0, cursor_line - 1, cursor_line, false)
+        if #lines == 0 then
+            break -- Exit the loop if there are no more lines
+        end
+        line_content = lines[1]
+        if not string.match(line_content, "|.*|$") then
+            break -- Exit the loop if the line does not match the table pattern
+        end
+        table.insert(file_lines, line_content)
+        cursor_line = cursor_line + 1
+    end
+
+    local table_bottom_line = cursor_line - 2
+
+    -- Check the line below the table for an .sc file link
+    local line_below_table = vim.api.nvim_buf_get_lines(0, table_bottom_line + 1, table_bottom_line + 2, false)[1] or ""
+
+    local _, sc_file_path = string.match(line_below_table, sc_file_link_pattern)
+
+    local temp_file_base = vim.fn.tempname()
+    local sc_file_base = vim.fn.expand('%:p:h') .. '/' .. generate_uuid() .. '.sc'
+    local md_file = temp_file_base .. '.md'
+    local sc_file = sc_file_path or sc_file_base
+    local scim_command
+    local sc_to_md_command = 'echo "EXECUTE \\"load ' ..
+        sc_file:gsub('"', '\\"') ..
+        '\\"\nEXECUTE \\"w! ' .. md_file:gsub('"', '\\"') .. '\\"" | sc-im --nocurses --quit_afterload'
+
+    if not sc_file_path and effective_config.include_sc_file then
+        -- No existing .sc file link found, create it from markdown
+        vim.fn.writefile(file_lines, md_file)
+        local script = 'EXECUTE "load ' ..
+            md_file:gsub('"', '\\"') .. '"\nEXECUTE "w! ' .. sc_file:gsub('"', '\\"') .. '"\n'
+        scim_command = 'echo "' .. script:gsub('"', '\\"') .. '" | sc-im'
+    else
+        -- Existing .sc file link found, use it
+        scim_command = 'sc-im ' .. sc_file:gsub('"', '\\"')
+    end
+
+
+    -- Save the current buffer number
+    local original_bufnr = vim.api.nvim_get_current_buf()
+
+    -- Create a new buffer for the terminal
+    local term_bufnr = vim.api.nvim_create_buf(true, false)
+
+    -- Open a new split and switch to the terminal buffer
+    vim.cmd('vsplit')
+    vim.api.nvim_win_set_buf(0, term_bufnr)
+
+    -- Run the sc-im command in the new buffer
+    vim.fn.termopen(scim_command, {
+        on_exit = function()
+            -- Run the scim_command and get its output (if needed)
+            local command_output = vim.fn.system(sc_to_md_command)
+
+            -- Optionally, you can check the output or handle errors
+            if vim.v.shell_error ~= 0 then
+                -- TODO not sure why I get a "No such devices or address" error here
+                -- but it seems to work anyway
+                -- print("Error: " .. command_output)
+            end
+            vim.api.nvim_buf_delete(term_bufnr, { force = true })
+
+            read_from_scim(table_top_line, table_bottom_line, md_file, sc_file, effective_config)
+        end
+    })
+
+    -- Start insert mode in the terminal
+    vim.cmd('startinsert')
+end
+
+-- Public function to setup the plugin
+function scim.setup(user_config)
+    -- Update configuration with user settings
+    for k, v in pairs(user_config or {}) do
+        config[k] = v
+    end
+end
+
+-- Public function to open in sc-im
+function scim.OpenInScim(override_config)
+    -- Merge the default config with any overrides
+    local effective_config = {}
+    for key, value in pairs(config) do
+        effective_config[key] = value
+    end
+    if override_config then
+        for key, value in pairs(override_config) do
+            effective_config[key] = value
+        end
+    end
+
+    open_in_scim(effective_config)
+end
+
+return scim
